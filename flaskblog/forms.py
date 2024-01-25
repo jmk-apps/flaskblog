3800ab3a4b1cab106aea5272163c00bbdfc26802
John Kaganda <jkaganda@gmail.com>
2024-01-25 13:57:27 +0200

    Added functionality to the account route and template.

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, EmailField, PasswordField, BooleanField, Submit
 from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError
 from flaskblog import db
 from flaskblog.models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -28,3 +29,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=2, max=20)])
+    email = EmailField('Email', validators=[InputRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = db.session.execute(db.select(User).where(User.username == username.data)).scalar()
+            if user:
+                raise ValidationError("That username is taken. Please choose a different one.")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = db.session.execute(db.select(User).where(User.email == email.data)).scalar()
+            if user:
+                raise ValidationError("That email is taken. Please choose a different one.")
